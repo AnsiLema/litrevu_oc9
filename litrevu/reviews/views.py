3899bace3df32025e773ee2cde37747c2b60975a
A'nsi Lema <ansilema@gmail.com>
2025-01-15 18:15:22 +0100

    Convert views to use templates and add base structure.
    Replaced static HTML in views with Django templates for better structure and maintainability. Introduced a `base.html` template and updated other templates to extend it. Updated `views.py` functions to render appropriate templates for consistent design and easier scalability.

--- litrevu/reviews/views.py
+++ litrevu/reviews/views.py
@@ -3,23 +3,14 @@ from django.http import HttpResponse
 from reviews.models import Review
 
 def hello(request):
-    reviews = Review.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{reviews[0].name}</li>
-            <li>{reviews[1].name}</li>
-            <li>{reviews[2].name}</li>
-            <li>{reviews[3].name}</li>
-        </ul>
-""")
+     return render(request, "reviews/hello.html")
 
 def about(request):
-    return HttpResponse("<h1>À propos</h1> <p>Nous adorons LITRevu !</p>")
+    return render(request, "reviews/about.html")
 
 def reviews(request):
-    return HttpResponse("<h1>Reviews</h1> <p>Voici nos avis !</p>")
+    reviews = Review.objects.all()
+    return render(request, "reviews/review.html", {"reviews": reviews})
 
 def contact(request):
-    return HttpResponse("<h1>Contact</h1> <p>Contactez-nous !</p>")
+    return render(request, "reviews/contact.html")
